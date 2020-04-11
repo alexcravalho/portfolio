10077398554dd3f79bd1b63dab2b2d026d439676
alexcravalho <a.d.cravalho@gmail.com>
2020-04-10 18:46:41 -0700

    imported and rendered Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Home from './Home';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      homeList: [
+        'RESUME',
+        'LINKEDIN',
+        'GITHUB',
+        '(530) 559-6101',
+        'a.d.cravalho@gmail.com'
+      ],
+      hrefList: [
+        'https://alexcravalho-portfolio.s3-us-west-2.amazonaws.com/AlexanderCravalhoResume.pdf',
+        'https://www.linkedin.com/in/alexcravalho',
+        'https://github.com/alexcravalho',
+        'tel:+15305596101',
+        'mailto:a.d.cravalho@gmail.com?subject=Connect%20with%20Alex%20Cravalho&body=Hello%20Alex&#44%0D%0AI%20found%20your%20email%20on%20your%20website%20and%20want%20to%20connect!'
+      ],
+      iconList: [
+        faFileDownload,
+        faLinkedin,
+        faGithubSquare,
+        faPhoneSquareAlt,
+        faPaperPlane
+      ]
 
     }
 
@@ -12,7 +40,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div>Hello from React!</div>
+        <Home homeList={this.state.homeList} hrefList={this.state.hrefList} iconList={this.state.iconList}/>
       </div>
     );
   }
