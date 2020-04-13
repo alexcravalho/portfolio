5787238eeaa79908c3f7482b64eb63f7ac8a5765
alexcravalho <a.d.cravalho@gmail.com>
2020-04-13 16:48:55 -0700

    created About functional component

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+const About = (props) => (
+  <div className="About">
+    <div className="heading">About Me</div>
+    <div>
+      Hi, I'm Alex! I'm a passionate problem solver, and creative, out-of-the box thinker. I am naturally curious and love breaking things down into their respective parts to figure out how they work. I love leveraging technlogy against modern problems to improve the quality of people's lives.
+    </div>
+    <br/>
+    <br/>
+    <div>
+      Right now, I am working as a freelance Software Engineer taking up contracts while I look for long term positions in Seattle. Companies or employers, please reach out to me about employment oppunities.
+    </div>
+    <br/>
+    <br/>
+    <div>
+      When I'm not programming, I love to travel, play my ukelele, and explore the outdoors.
+    </div>
+    <div>Pictures</div>
+  </div>
+);
+
+export default About;
