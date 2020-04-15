4bb7ca7642cf6336518eb25cb3cdf14af6b27dd1
alexcravalho <a.d.cravalho@gmail.com>
2020-04-15 15:31:58 -0700

    edited img src green

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -6,10 +6,10 @@ const Recent = (props) => (
     <div className='heading'>Recent Engineering Work</div>
     <div className="application-list">
       <div className="application-item">
-        <img className='recent-img' src='/images/green.webp' alt='think green'></img>
+        <img className='recent-img' src='/images/green.webp' alt='green'></img>
         <div className="recent-text-container">
           <div className="recent-title">GreenPDF Internal Tool</div>
-          <div className="recent-skills">React | TypeScript| Express | Node | Material-UI | Puppeteer</div>
+          <div className="recent-skills">React | TypeScript | Express | Node | Material-UI | Puppeteer</div>
           <div className="recent-description">I am currently building an internal tool for a small landscaping company that wants to update its business to include eco-friendly practices. The owner of the company wants to remove all paper records and move the company to a fully digital platform. This application captures relevant company data, transforms it into a beautiful PDF document and emails it to the company's cloud storage system at the click of a button.</div>
           <div className="recent-button-container">
             <button>View</button>
@@ -21,7 +21,7 @@ const Recent = (props) => (
       <img className='recent-img' src='/images/crashlogo.webp' alt='crash logo'></img>
         <div className="recent-text-container">
           <div className="recent-title">Crash Stats</div>
-          <div className="recent-skills">React | React Hooks | JavaScript | Express | Node| MongoDB | AWS | Docker</div>
+          <div className="recent-skills">React | React Hooks | JavaScript | Express | Node | MongoDB | AWS | Docker</div>
           <div className="recent-skills"></div>
           <div className="recent-description">Love Hockey? I do too! I created this application for sports buffs who want an edge in their Fantasy Hockey season. Want to know if a particular fantasy trade is worth it? This app can evaluate a particular player trade based on real game statistics from the NHL public API. It also allows you to input your own metrics for each category, which varies across fantasy leagues.</div>
           <div className="recent-button-container">
