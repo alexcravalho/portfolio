90da01fee109a379e0c9e5e918e9ea13858c5fdc
alexcravalho <a.d.cravalho@gmail.com>
2020-04-14 17:03:28 -0700

    created eductation functional component

---
+++ src/components/Education.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const Education = (props) => (
+  <div className='Education'>
+    <div className='heading'>Education</div>
+  </div>
+);
+
+export default Education;
