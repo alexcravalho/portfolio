a2f2e72939104b7ad50d9b62044bf58048f2edf6
alexcravalho <a.d.cravalho@gmail.com>
2020-04-14 17:03:46 -0700

    created experience functional component

---
+++ src/components/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const Experience = (props) => (
+  <div className='Experience'>
+    <div className='heading'>Experience</div>
+  </div>
+);
+
+export default Experience;
