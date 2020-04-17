8f6015646d5ec87bcd40819119c8f464397a170e
alexcravalho <a.d.cravalho@gmail.com>
2020-04-16 18:04:41 -0700

    added text and styling to Experience functional component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,6 +4,17 @@ import './App.css';
 const Experience = (props) => (
   <div className='Experience'>
     <div className='heading'>Experience</div>
+    <div className='job-container'>
+      {props.jobList.map((job, idx) => (
+        <div className='job-description' key={idx}>
+          <div className='job-header'>
+            <span className='job-title'>{job}</span>
+            <span className='job-date'>{props.jobDates[idx]}</span>
+          </div>
+          <div className='job-text'>{props.jobDes[idx]}</div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
