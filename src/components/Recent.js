13901c456b747890f0d3ec9620a5d21a59dd7788
alexcravalho <a.d.cravalho@gmail.com>
2020-04-20 18:04:35 -0700

    edited heading text; converted buttons to div elements;

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const Recent = (props) => (
   <div className='Recent'>
-    <div className='heading'>Recent Engineering Work</div>
+    <div className='heading'>Work Experience</div>
     <div className="application-list">
       <div className="application-item">
         <img className='recent-img' src='/images/green.webp' alt='green'></img>
@@ -12,8 +12,8 @@ const Recent = (props) => (
           <div className="recent-skills">React | TypeScript | Express | Node | Material-UI | Puppeteer</div>
           <div className="recent-description">I am currently building an internal tool for a small landscaping company that wants to update its business to include eco-friendly practices. The owner of the company wants to remove all paper records and move the company to a fully digital platform. This application captures relevant company data, transforms it into a beautiful PDF document and emails it to the company's cloud storage system at the click of a button.</div>
           <div className="recent-button-container">
-            <button>View</button>
-            <button>Github</button>
+            <div className='button'>View</div>
+            <div className='button'>Github</div>
           </div>
         </div>
       </div>
@@ -25,8 +25,8 @@ const Recent = (props) => (
           <div className="recent-skills"></div>
           <div className="recent-description">Love Hockey? I do too! I created this application for sports buffs who want an edge in their Fantasy Hockey season. Want to know if a particular fantasy trade is worth it? This app can evaluate a particular player trade based on real game statistics from the NHL public API. It also allows you to input your own metrics for each category, which varies across fantasy leagues.</div>
           <div className="recent-button-container">
-            <button>View</button>
-            <button>Github</button>
+          <div className='button'>View</div>
+            <div className='button'>Github</div>
           </div>
         </div>
       </div>
@@ -37,8 +37,8 @@ const Recent = (props) => (
           <div className="recent-skills">React | JavaScript | Express | Node | PostgreSQL | Cassandra | Nginx | Docker | Loader.io | New Relic</div>
           <div className="recent-description">Everyone listens to music! This app will be your go-to destination to listen to all the latest hits. I built out the back end of this application to scale for high-web traffic. I completely replaced and scaled the back-end of the Related Tracks service to handle 55 million+ records. I improved efficiency by optimizing database query speed from 3 seconds to less than 25 milliseconds. I designed and engineered the system architecture to include multiple servers and load balancer, increasing throughput by 33%. </div>
           <div className="recent-button-container">
-            <button>View</button>
-            <button>Github</button>
+          <div className='button'>View</div>
+            <div className='button'>Github</div>
           </div>
         </div>
       </div>
