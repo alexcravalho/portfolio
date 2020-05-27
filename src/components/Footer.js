db57f1832aff9a2303703fe26146ce7ff2155caa
alexcravalho <a.d.cravalho@gmail.com>
2020-05-26 18:31:23 -0700

    added alt property to a tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ import './App.css';
 const Footer = (props) => (
   <div className='footer'>
     <div className='back-to-top'>
-      <a href='http://localhost:3000/#'>Back to Top!</a>
+      <a href='http://localhost:3000/#' alt='back-to-top'>Back to Top!</a>
     </div>
     <div className='bottom-panel'>
       <div className='project-link'>
         <span className='link-text'>Constructed with React.js</span>
         <div className='footer-button'>
-          <a href='https://github.com/alexcravalho/portfolio' target='_blank' rel='noopener noreferrer'>
+          <a href='https://github.com/alexcravalho/portfolio' alt='portfolio-github-link' target='_blank' rel='noopener noreferrer'>
             <FontAwesomeIcon icon={props.iconList[2]}/>
           </a>
         </div>
@@ -19,7 +19,7 @@ const Footer = (props) => (
       <div className='like-page-text'>Like This Page?</div>
       <div className='footer-icon-holder'>
         {props.homeList.map((icon, idx) => (
-          <a href={props.hrefList[idx]} target='_blank' rel='noopener noreferrer' key={idx}>
+          <a href={props.hrefList[idx]} alt={icon} target='_blank' rel='noopener noreferrer' key={idx}>
             <span className='icon-button'>
               <FontAwesomeIcon icon={props.iconList[idx]}/>
             </span>
