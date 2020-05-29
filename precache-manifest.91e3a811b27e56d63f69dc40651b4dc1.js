3b98aa597e268f0c012bd4a393ea38e30164ec7e
alexcravalho <a.d.cravalho@gmail.com>
2020-05-28 20:19:24 -0700

    Updates

---
+++ precache-manifest.91e3a811b27e56d63f69dc40651b4dc1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323a872e8a3b916b1ac32b3644b8543b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0e75d8657f7ccd9dfa29",
+    "url": "/portfolio/static/css/main.2c10eefc.chunk.css"
+  },
+  {
+    "revision": "1070f521addfd7ebe684",
+    "url": "/portfolio/static/js/2.a184f2d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a184f2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e75d8657f7ccd9dfa29",
+    "url": "/portfolio/static/js/main.4f0dd6ca.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "317c83ea61eab172f8a1dad69e286ebe",
+    "url": "/portfolio/static/media/Ailerons-Regular.317c83ea.otf"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/portfolio/static/media/Anurati-Regular.ca8632f4.otf"
+  },
+  {
+    "revision": "f5143574eff9205c05b597b49761621d",
+    "url": "/portfolio/static/media/Exan-Regular.f5143574.ttf"
+  },
+  {
+    "revision": "905c77bf2b36d23605a1823e48c469d9",
+    "url": "/portfolio/static/media/Michroma-Regular.905c77bf.ttf"
+  },
+  {
+    "revision": "50921cbc5b6d1cc2465cfd73af8a1fdb",
+    "url": "/portfolio/static/media/Simple-Regular.50921cbc.ttf"
+  }
+]);
