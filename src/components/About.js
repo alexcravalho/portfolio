4a1344e7e87027ea61bbd10323332f5b584ba055
alexcravalho <a.d.cravalho@gmail.com>
2020-06-03 12:07:57 -0700

    edited .about-text

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ const About = (props) => (
   <div className='About'>
     <div className='heading'>About Me</div>
     <div className='about-text'>
-      Hi, I'm Alex! I'm a passionate problem solver and creative, out-of-the box, thinker. I am naturally curious and love breaking things down into their respective parts to figure out how they work. I love leveraging technlogy against modern problems to improve the quality of people's lives.
+      Hi, I'm Alex! I'm a passionate problem solver and creative, out-of-the box, thinker. I am naturally curious and love breaking things down into their respective parts to figure out how they work. I love leveraging technology against modern problems to improve the quality of people's lives.
     </div>
     <div className='about-text'>
-      Right now, I am working as a freelance Software Engineer taking up contracts while I look for long term positions in Seattle. Companies or employers, please reach out to me about employment oppunities.
+      Right now, I am working as a freelance Software Engineer taking up contracts while I look for long term positions in Seattle. Companies or employers, please reach out to me about work opportunities.
     </div>
     <div className='about-text'>
-      When I'm not programming, I love to travel, explore the outdoors, play my ukelele, and play just about any kind of game.
+      When I'm not programming, I love to travel, explore the outdoors, play my ukelele, and enjoy just about any kind of game.
     </div>
     <div className='about-pic-row'>
       {props.aboutImgList.map((pic, idx) => (
