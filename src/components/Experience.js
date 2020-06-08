b9fdd3d49fd9a1163eaaaa9c48a3c360250be771
alexcravalho <a.d.cravalho@gmail.com>
2020-06-08 12:49:53 -0700

    added .job-position

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,6 +9,7 @@ const Experience = (props) => (
         <div className='job-description' key={idx}>
           <div className='job-header'>
             <div className='job-title'>{job}</div>
+            <div className='job-position'>{props.jobTitles[idx]}</div>
             <div className='job-date'>{props.jobDates[idx]}</div>
           </div>
           <div className='job-text'>{props.jobDes[idx]}</div>
